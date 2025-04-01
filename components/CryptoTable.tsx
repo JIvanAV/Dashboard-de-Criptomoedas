c2b3e06aa374ed76001a0ed455e4b5b4a5e97cd1
J. Ivan A.V. <60518463+JIvanAV@users.noreply.github.com>
2025-04-01 13:38:47 -0300

    Update CryptoTable.tsx

--- components/CryptoTable.tsx
+++ components/CryptoTable.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface CryptoTableProps {
   data: any[];
@@ -21,7 +22,9 @@ export default function CryptoTable({ data }: CryptoTableProps) {
             <td className="border border-gray-700 p-2">{index + 1}</td>
             <td className="border border-gray-700 p-2 flex items-center gap-2">
               <Image src={coin.image} alt={coin.name} width={24} height={24} />
-              {coin.name}
+              <Link href={`/coin/${coin.id}`} className="hover:underline text-blue-400">
+                {coin.name}
+              </Link>
             </td>
             <td className="border border-gray-700 p-2">${coin.current_price.toFixed(2)}</td>
             <td
