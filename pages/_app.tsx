2a6fdea37a3c2087ae6b5c1f0240677c44b8424c
J. Ivan A.V. <60518463+JIvanAV@users.noreply.github.com>
2025-04-01 13:49:37 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { FavoritesProvider } from "@/context/FavoritesContext";
+import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <FavoritesProvider>
+      <Navbar />
       <Component {...pageProps} />
     </FavoritesProvider>
   );
